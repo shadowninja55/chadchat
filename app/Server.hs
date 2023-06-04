7a6ce8cb44dca72bb9f88de6eb9e61e7cd9e5448
shadowninja55 <shadowninja1050@gmail.com>
2023-06-04 11:50:12 -0400

    fix server concurrency and log disconnections

--- app/Server.hs
+++ app/Server.hs
@@ -3,7 +3,7 @@ module Main where
 
 import ChadChat.Message
 import Codec.Serialise (serialise)
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, killThread)
 import Control.Concurrent.Chan
 import Control.Logging qualified as L
 import Control.Monad
@@ -20,16 +20,19 @@ handleClient messages conn = do
   Just (toString -> username) <- S.read $ source conn
   let (_, addr) = connExtraInfo conn
   L.log . T.pack $ show addr <> " connected"
-  void . forkIO $ forever do
+  thread <- forkIO $ forever do
     (message, sender) <- readChan messages
-    when (addr /= sender) $
+    when (sender /= addr) $
       send conn $ serialise message
   fix \loop -> S.read (source conn) >>= \case
     Just (toString -> content) -> do
       L.debug . T.pack . display $ Message (show addr) content
       writeChan messages (Message username content, addr)
       loop
-    Nothing -> close conn
+    Nothing -> do
+      close conn
+      killThread thread
+      L.log . T.pack $ show addr <> " disconnected"
 
 main :: IO ()
 main = L.withStdoutLogging do
@@ -39,6 +42,6 @@ main = L.withStdoutLogging do
   forever do
     conn <- accept sock
     messages' <- dupChan messages
-    handleClient messages' conn
+    forkIO $ handleClient messages' conn
  where
   port = 5555
